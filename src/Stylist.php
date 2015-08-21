a0035ca9180386f06c93d37f4a57d7d54434c335
creightonja <creightonja@gmail.com>
2015-08-21 10:53:12 -0700

    create initial files and db

---
+++ src/Stylist.php
@@ -0,0 +1,15 @@
+<?php
+    require_once "Client.php";
+
+    class Stylist
+        {
+        private $stylist_name;
+        private $id;
+
+        function __construct($stylist_name, $id = null)
+        {
+            $this->stylist_name = $restaurant_name;
+            $this->id = $id;
+        }
+
+    
